48920069a9002ccd8242c1a5e99d2cb15d007a2e
Odil-Programming <odilchikfurqatovich@gmail.com>
2024-03-26 10:03:43 +0500

    string

---
+++ main.js
@@ -0,0 +1,27 @@
+// let odilName = prompt('Ismingizni kiriting');
+// let caseLower = odilName.caseLower();
+// alert(Ismingiz: ${caseLower});
+
+// let nom = (Harf kiriting:);
+// alert(kiriti;gan harf: ${harf});
+
+
+// let ikkinchiHarfIndex = lowercaseName.indexOf(harf, 1); 
+//     if (ikkinchiHarfIndex !== -1) {
+//         alert(${odilName} harfi ismning ichida mavjud.);
+//     } 
+    
+    
+//     else {
+//         alert(${caseLower} harfi ismning ichida mavjud emas.);
+//     }
+
+
+let odilName = prompt(`ismingizni kiriting`)
+let caseLower = prompt(`harf kiriting`)
+if (userName.toLowerCase().includes(letter.toLowerCase())) {
+    alert("Ha, ismingizda " + letter + " harfi bor!");
+} 
+
+else {
+    alert("Yo'q, ismingizda " + letter + " harfi yo'q!"); }
